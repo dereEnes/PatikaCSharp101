8b35edb6d19513b3f13b00b6755391a26509c556
dereEnes <78616137+dereEnes@users.noreply.github.com>
2021-12-16 19:53:49 +0300

    Add files via upload

---
+++ switchyapisi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace switchyapisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasınız");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış değer");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış değer");
+                    break;
+            }
+        }
+    }
+}
