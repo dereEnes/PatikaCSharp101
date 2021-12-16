f7c0353bb69ccddf89f0d2023ef56d2040ca9d3b
dereEnes <78616137+dereEnes@users.noreply.github.com>
2021-12-16 17:22:21 +0300

    Add files via upload

---
+++ variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            byte not = 75;
+            uint nufus = 83000000;
+            long dunyaNufusu = 8000000000;
+            bool isPass = true;
+
+            char basHarfi = 'E';
+            string name = "enes";
+
+            float maas = 4000.00f;
+            double birikim = 800000.00d;
+            short distinct = 12000;
+
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.ToString());
+            Console.WriteLine(date.ToString("dddd/MMMM/yyyy"));
+            Console.WriteLine(date.ToString("dd/MM/yy"));
+            Console.WriteLine(date.ToString("HH:mm"));
+
+            object o = 'd';
+            object o2 = 4;
+
+            var deneme = "enes";
+            
+
+        }
+    }
+}
