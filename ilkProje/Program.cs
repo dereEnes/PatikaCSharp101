0defdafffb95d24ac0adb7a6fa14f5ace0cb12c3
dereEnes <78616137+dereEnes@users.noreply.github.com>
2021-12-16 16:36:18 +0300

    Add files via upload

---
+++ ilkProje/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace java101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Girin");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba" + name+ " " + surname);
+        }
+    }
+}
