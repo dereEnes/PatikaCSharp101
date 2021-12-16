9b2bf9f38ab7c13f30ece2bd356b35caa60e0f68
dereEnes <78616137+dereEnes@users.noreply.github.com>
2021-12-16 18:09:39 +0300

    Add files via upload

---
+++ hataYonetimi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace hataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Bir sayi giriniz : ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi : "+sayi);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message.ToString());
+                throw;
+            }finally{
+                Console.WriteLine("finally çalıştı");
+            }
+            
+             // try 
+            // {
+            //     // int a = int.Parse(null);
+            //     // int a = int.Parse("test");
+            //     int a = int.Parse("-200000000000"); 
+            // }
+
+            //  catch (ArgumentNullException ex)
+            // {
+            //     Console.WriteLine("Boş değer girdiniz.");
+            //     Console.WriteLine(ex);
+            // }
+
+            // catch (FormatException ex)
+            //  {
+            //      Console.WriteLine("Veri tipi uygun değil.");
+            //      Console.WriteLine(ex);
+            //  }
+
+            // catch (OverflowException ex)
+            // {
+            //     Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz");
+            //     Console.WriteLine(ex);
+            // }
+
+            // finally
+            // {
+               //  Console.WriteLine("işlem gerçekleştirildi.");
+            // }
+        }
+    }
+}
