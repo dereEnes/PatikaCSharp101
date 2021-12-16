876df0b4b2443b2b9329c7cfc5bb53229aaa0142
dereEnes <78616137+dereEnes@users.noreply.github.com>
2021-12-16 20:59:33 +0300

    Add files via upload

---
+++ forLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace forLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz:");
+            int sayac = int.Parse(Console.ReadLine().Trim());
+            for (int i = 0; i < sayac; i++)
+            {
+                if (i%2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
