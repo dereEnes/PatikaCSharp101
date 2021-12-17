8eab57708fb4eabf4beeb128f7a25df2e672f5fc
dereEnes <78616137+dereEnes@users.noreply.github.com>
2021-12-17 21:36:06 +0300

    Add files via upload

---
+++ arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"kedi", "kuş", "köpek"};
+
+            int[] dizi;
+            dizi = new int[6];
+
+            //değer atama ve erişim
+
+            dizi[0] = 2;
+
+            renkler[0]  = "sarı";
+
+            Console.Write(hayvanlar[0]);
+
+            
+        }
+    }
+}
