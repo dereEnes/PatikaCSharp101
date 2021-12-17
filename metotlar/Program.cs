9044caae2963b6469fbd0586166fedb318adc8a3
dereEnes <78616137+dereEnes@users.noreply.github.com>
2021-12-17 17:32:59 +0300

    Add files via upload

---
+++ metotlar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(Topla(a,b));
+        }
+        static int Topla(int a, int b){
+            return (a + b);
+        }
+    }
+}
