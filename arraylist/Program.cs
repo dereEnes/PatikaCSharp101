ea2a11ef5cfc31c3e8882f15771cf3b800346638
dereEnes <78616137+dereEnes@users.noreply.github.com>
2021-12-18 18:13:34 +0300

    Add files via upload

---
+++ arraylist/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+namespace arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //birder fazla veri tipini barındırabilir. dinamik büyür kücür System.Collections
+            ArrayList liste = new ArrayList();
+            liste.Add("Ayşe");
+            liste.Add(2);
+            liste.Add(false);
+            
+            //Toplu halde değerler ekleme
+            ArrayList deneme = new ArrayList();
+            deneme.Add('a');
+            deneme.Add(5.6);
+            liste.AddRange(deneme);
+
+            // *Sıralama için tiplerin aynı olması gerek
+            ArrayList deneme2 = new ArrayList();
+            deneme2.Add(true);
+            deneme2.Add(false);
+            deneme2.Add(true);
+            deneme2.Sort();
+
+            liste.Reverse();
+            
+            liste.Clear();
+        }
+    }
+}
